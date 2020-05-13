b6d06459fa58b4763fda7566e1a6d3af00619c15
bemijonathan <bemijonathan@gmail.com>
2020-05-13 19:32:40 +0100

    fix port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import server from './sever'
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log("the server is listening on " + port)
