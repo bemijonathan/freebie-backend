7d8dbc01f31d6b79b9f9e82b5eb9ef3c8a2e22f3
bemijonathan <bemijonathan@gmail.com>
2020-06-08 06:16:40 +0100

    understood the difference between integration test and unit test

--- src/resources/users/__tests__/user.controller.spec.js
+++ src/resources/users/__tests__/user.controller.spec.js
@@ -4,23 +4,32 @@ import response from "res";
 const { getOne, updateOne, getMany } = controller;
 
 describe("Crud action on users", () => {
-	test("create user should return status 200", async () => {
-		const req = {
-			params: {
-				id: 1,
-			},
-		};
+	// test("create user should return status 200", async () => {
 
-		const getOne = jest.fn();
-
-		const res = new response();
-
-		const StatusSpy = jest.spyOn(res, "status");
-		const JsonSpy = jest.spyOn(res, "json");
-
-		await getOne(req, res);
-
-		expect(getOne).toHaveBeenCalled();
-		expect(res).toHaveBeenCalled();
-	});
+		const b = 1+1
+		test("1+1 should equal 2", () => {
+			expect(b).toEqual(2)
+		})
+		// const req = {
+		// 	params: {
+		// 		id: 1,
+		// 	},
+		// };
+		// difference between a mock and a spy
+		// /*
+		// * a mock is data spy actually looks to see if somthing is called mock a function prevents the actual function
+		// * from being called
+		// */
+		// const getOne = jest.fn();
+		//
+		// const res = new response();
+		//
+		// const StatusSpy = jest.spyOn(res, "status");
+		// const JsonSpy = jest.spyOn(res, "json");
+		//
+		// await getOne(req, res);
+		//
+		// expect(getOne).toHaveBeenCalled();
+		// expect(res).toHaveBeenCalled();
+	// });
 });
